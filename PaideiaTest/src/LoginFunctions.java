928265b5165fab77c7dcfa31f2f31196dfaab04a
Sadman Rahman Mridul <86281823+sadman-rahman-mridul@users.noreply.github.com>
2023-05-16 02:59:28 +0600

    Homepage Updated

--- PaideiaTest/src/LoginFunctions.java
+++ PaideiaTest/src/LoginFunctions.java
@@ -1,10 +1,10 @@
 //this class contains all loginpage components (methods and attributes)
 
-import javax.swing.*; 
+import javax.swing.*;
 //import java.awt.event.*;
 
-public class LoginFunctions implements ILoginFunctions{
-    //loginpage components
+public class LoginFunctions implements ILoginFunctions {
+    // loginpage components
     JLabel Username, Email, Password;
     JTextField UsernameTF, EmailTF;
     JPasswordField PasswordPF;
@@ -12,47 +12,49 @@ public class LoginFunctions implements ILoginFunctions{
     JButton WithEmailB, WithUsernameB;
     JButton Button, sgnpButton;
 
-    public LoginFunctions(){
-        //creating objects for loginpage components
-            //labels
+    public LoginFunctions() {
+        // creating objects for loginpage components
+        // labels
         Username = new JLabel("Username: ");
         Email = new JLabel("Email: ");
         Password = new JLabel("Password: ");
-            //textfields
+        // textfields
         UsernameTF = new JTextField();
         EmailTF = new JTextField();
         PasswordPF = new JPasswordField();
-            //buttons
+        // buttons
         ForgotPasswordB = new JButton("Forgot Password?");
         WithEmailB = new JButton("Login with email");
         WithUsernameB = new JButton("Login with Username");
         Button = new JButton("Login");
-        sgnpButton = new JButton("Don't have an account?");
+        sgnpButton = new JButton("Create Account");
     }
 
-    public void loginWithEmail(JFrame mainFrame, LoginFunctions lgn){
-        //designing
-        /*changing the source button
-        *removing username label and textfield
-        *addming email label and textfield
-        */
+    public void loginWithEmail(JFrame mainFrame, LoginFunctions lgn) {
+        // designing
+        /*
+         * changing the source button
+         * removing username label and textfield
+         * addming email label and textfield
+         */
         mainFrame.remove(lgn.WithEmailB);
         mainFrame.add(lgn.WithUsernameB);
         mainFrame.remove(lgn.Username);
         mainFrame.remove(lgn.UsernameTF);
         mainFrame.add(lgn.Email);
         mainFrame.add(lgn.EmailTF);
-        
-        //refreshing mainframe
+
+        // refreshing mainframe
         SwingUtilities.updateComponentTreeUI(mainFrame);
     }
 
-    public void loginwithUsername(JFrame mainFrame, LoginFunctions lgn){
-        //designing
-        /*changing the source button
-        *removing email label and textfield
-        *adding username label and textfield
-        */
+    public void loginwithUsername(JFrame mainFrame, LoginFunctions lgn) {
+        // designing
+        /*
+         * changing the source button
+         * removing email label and textfield
+         * adding username label and textfield
+         */
         mainFrame.add(lgn.WithEmailB);
         mainFrame.remove(lgn.WithUsernameB);
         mainFrame.remove(lgn.Email);
@@ -60,7 +62,7 @@ public class LoginFunctions implements ILoginFunctions{
         mainFrame.add(lgn.Username);
         mainFrame.add(lgn.UsernameTF);
 
-        //refreshing mainframe
+        // refreshing mainframe
         SwingUtilities.updateComponentTreeUI(mainFrame);
     }
 }
